528d39c028459d70a6640641db2b94bed547582e
paliwalarpan <arpan.paliwal12@gmail.com>
2021-01-31 17:59:41 +0530

    enabled swagger

--- src/main/java/com/club/eliteclub/EliteClubApplication.java
+++ src/main/java/com/club/eliteclub/EliteClubApplication.java
@@ -9,10 +9,12 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class EliteClubApplication implements ApplicationRunner {
     private static final Logger LOG = LoggerFactory.getLogger(EliteClubApplication.class);
 
