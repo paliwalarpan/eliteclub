ede830bbfcc14e650b72f3f25dbb4c35f3d304f6
paliwalarpan <arpan.paliwal12@gmail.com>
2021-04-17 16:50:01 +0530

    enabled swagger....

--- src/main/java/com/club/eliteclub/EliteClubApplication.java
+++ src/main/java/com/club/eliteclub/EliteClubApplication.java
@@ -2,8 +2,10 @@ package com.club.eliteclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class EliteClubApplication {
 
     public static void main(String[] args) {
